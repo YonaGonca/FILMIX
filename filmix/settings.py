3b6e21a792f9ee8aa76ce9603436aa0ac541e367
YonaGonca <ygoncalves5a@gmail.com>
2025-02-23 00:47:02 +0000

    -JWT Authentication config done -User model with roles created

--- filmix/settings.py
+++ filmix/settings.py
@@ -129,3 +129,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#JWT AUTH
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+AUTH_USER_MODEL = 'users.CustomUser'
